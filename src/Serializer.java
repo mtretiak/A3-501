5267ce8fff651459f83728f1b92ec605e9acefb1
Michael Treitak <mtretiak@me.com>
2017-11-11 21:53:08 -0700

    FIRST PUSSSH
    first push. nothing done but jDom import

---
+++ src/Serializer.java
@@ -0,0 +1,14 @@
+import org.jdom2.Document;
+
+/**
+ * Created by mtretiak on 2017-11-11.
+ */
+public class Serializer {
+
+
+    public static Document serializeObj(Object obj){
+
+        
+
+    }
+}
